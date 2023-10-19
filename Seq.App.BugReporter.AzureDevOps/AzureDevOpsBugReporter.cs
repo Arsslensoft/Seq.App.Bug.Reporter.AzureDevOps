fa365f108a77508f41ec4295a92b6106ad52a1ed
Arsslen Idadi <arsslens021@gmail.com>
2023-10-19 14:36:57 -0400

    Initial

---
+++ Seq.App.BugReporter.AzureDevOps/AzureDevOpsBugReporter.cs
@@ -0,0 +1,15 @@
+﻿using Seq.Apps.LogEvents;
+using Seq.Apps;
+
+namespace Seq.App.BugReporter.AzureDevOps
+{
+    [SeqApp("Azure DevOps Bug Reporter",
+        Description = "TBD.")]
+    public class AzureDevOpsBugReporter : SeqApp, ISubscribeToAsync<LogEventData>
+    {
+        public async Task OnAsync(Event<LogEventData> evt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
